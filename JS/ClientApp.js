527e0afaa2c6d49b8e82a4c39cf334357163264d
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-09-22 19:30:14 -0400

    Create 1st component

---
+++ JS/ClientApp.js
@@ -0,0 +1,26 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+	render () {
+		return (
+			div(null, 
+				h1(null, "Checkout this other thing.")
+			)
+		)
+	}
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+var MyFirstComponent = (
+  
+  div(null,
+  	MyTitleFact(null),
+  	React.createElement(MyTitle, null),
+  	React.createElement(MyTitle, null)
+  )
+)
+
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
