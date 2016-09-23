e7890fffca5732c9dd2e48bd560caee93baaee51
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-09-22 23:48:06 -0400

    Git issues - resolved

--- JS/ClientApp.js
+++ JS/ClientApp.js
@@ -1,26 +1,26 @@
+/* global React ReactDOM */
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-	render () {
-		return (
-			div(null, 
-				h1(null, "Checkout this other thing.")
-			)
-		)
-	}
+  render () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
 })
 
 var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
 var MyFirstComponent = (
-  
   div(null,
-  	MyTitleFact(null),
-  	React.createElement(MyTitle, null),
-  	React.createElement(MyTitle, null)
+    MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
+    ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
   )
 )
-
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
