b51ece887ea3facda7968323b1745e830b0340d5
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-09-24 02:25:26 -0400

    Render images and text

--- JS/ClientApp.jsx
+++ JS/ClientApp.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const { Router, Route, hashHistory } = require('react-router')
+const Search = require('./Search')
 
 // this is a function expression for quick display to screen
 // if I need to declare a variable (const) then add return back
@@ -9,6 +10,7 @@ const { Router, Route, hashHistory } = require('react-router')
 const App = () => ( 
   <Router history ={hashHistory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 
