e5bc9f0f90ad0b9f12e67abff53f39f67150c757
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-10-15 17:36:39 -0400

    Refactored Search.jsx

--- JS/Search.jsx
+++ JS/Search.jsx
@@ -4,14 +4,38 @@ const data = require('../public/data')
 
 // Spread Operator {...show} grabs all properties of show
 
-const Search = (props) => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+// another format is class Search extends React.Component
+
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value})
+  },
+  render () {
+    return (    
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm} className='search-input' type=
+            'text' placeholder='Search' onChange={this.handleSearchTermEvent} 
+            />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) => `${show.title} ${show.description}`
+              .toUpperCase()
+              .indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
