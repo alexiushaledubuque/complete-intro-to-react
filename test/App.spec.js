d44a62e286ffe63db5574d9ba4f2994aacb4918e
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-10-17 16:05:07 -0400

    Testing

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,1 +1,23 @@
-App.spec.js
+/* eslint-env mocha */
+
+const { expect } = require('chai')
+
+// describe('Testing <Search /> component', () => {
+//   it('should pass', () => {
+//     expect(1 + 1 === 2).to.be.true
+//   })
+// })
+
+// describe('Testing <Search /> component', () => {
+//   it('should pass', () => {
+//     expect(1 + 1).to.equal(5)
+//   })
+// })
+
+
+// putting the 'x' in front of it comments out the test
+describe('Testing <Search /> component', () => {
+  it('should pass', () => {
+    expect(1 + 1).to.equal(2)
+  })
+})
