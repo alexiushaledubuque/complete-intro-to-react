d006550d6c36c92683e772815502a7b151bf9d89
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-10-25 19:17:57 -0400

    AJAX Requests in React

--- JS/ClientApp.jsx
+++ JS/ClientApp.jsx
@@ -6,10 +6,19 @@ const Search = require('./Search')
 const Layout = require('./Layout')
 const Details = require('./Details')
 
-// Destructuring { Router, Route, hashHistory }
-const { Router, Route, IndexRoute, hashHistory } = require('react-router')
+// Destructuring { Router, Route, browserHistory }
+const { Router, Route, IndexRoute, browserHistory } = require('react-router')
 const { store } = require('./Store')
 const { Provider } = require('react-redux')
+
+// Function that returns a set of routes
+const myRoutes = () => (
+  <Route path='/' component={Layout}>
+    <IndexRoute component={Landing} />
+    <Route path='/search' component={Search} />
+    <Route path='/details/:id' component={Details} />
+  </Route>
+)
 
 
 // 'const App = () => ()' is a function expression for quick display to screen
@@ -23,16 +32,14 @@ const App = React.createClass({
   render () {
     return (
       <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
-          </Route>
+        <Router history={browserHistory}>
+          {myRoutes()}
         </Router>
       </Provider>
     )
   }
 })
+
+App.Routes = myRoutes
 
 module.exports = App
--- JS/Details.jsx
+++ JS/Details.jsx
@@ -1,8 +1,28 @@
 const React = require('react')
 const Header = require('./Header')
+const axios = require('axios')
 const { connector } = require('./Store')
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      omdbData: {}
+    }
+  }
+  componentDidMount() {
+    console.log('here', this.assignShow(this.props.params.id).imdbID)
+
+    axios.get(`http://www.omdbapi.com/?i${this.assignShow(this.props.params.id).imdbID}`)
+      .then((response) => {
+        this.setState({omdbData: response.data})
+      })
+      .catch((error) => {
+        console.error('axios error', error)
+      })
+  }
+
   assignShow (id) {
     // notes are incorrect
     const showArray = this.props.shows.filter((show) => show.imdbID === id )
@@ -11,13 +31,18 @@ class Details extends React.Component {
   render () {
     const { title, description, year, poster, trailer } = this.assignShow(this.props.params.id)
 
+    let rating
+    if(this.state.omdbData.imdbRating) {
+      rating = <h3 className='video-rating'>{this.state.omdbData.imdbRating}</h3>
+    }
     return (
       <div className='container'>
         <Header />
         <div className='video-info'>
           <h1 className='video-title'>{title}</h1>
           <h1 className='video-year'>({year})</h1>
-          <img className='video-poster' src={`public/img/posters/${poster}`} />
+            {rating}
+          <img className='video-poster' src={`/public/img/posters/${poster}`} />
           <p className='video-description'>{description}</p>
         </div>
         <div className='video-container'>
